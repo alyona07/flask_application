2e90e54f32eced4fd11e6dc33741e13e7f39a359
alyonajun <alyoniha@gmail.com>
2014-02-25 17:44:55 +0200

    new function

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = {
+            "title": "Super Test",
+            "header": "HE-HE-HE-HE... Hello World!!",
+            "lines": [
+                {"title": "Row_0", "columns": ["first_00", "first_01", "first_02", "first_03"]},
+                {"title": "Row_1", "columns": ["second_10", "second_11", "second_12", "second_13"]},
+                {"title": "Row_2", "columns": ["third_20", "third_21", "third_22", "third_23"]},
+                {"title": "Row_3", "columns": ["fourth_30", "fourth_31", "fourth_32", "fourth_33"]},
+            ]
+    }
+    return render_template('index.html', data=data)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="localhost", port=5000)
+
