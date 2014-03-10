7411865525ff4d059812dfcb0c0cdb2c7c1cf52a
alyonajun <alyoniha@gmail.com>
2014-03-10 20:01:49 +0200

    new developments

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -13,12 +13,13 @@ def index():
             "title": "Super Test",
             "header": "HE-HE-HE-HE... Hello World!!",
             "lines": [
-                {"title": "Row_0", "columns": ["first_00", "first_01", "first_02", "first_03"]},
-                {"title": "Row_1", "columns": ["second_10", "second_11", "second_12", "second_13"]},
-                {"title": "Row_2", "columns": ["third_20", "third_21", "third_22", "third_23"]},
-                {"title": "Row_3", "columns": ["fourth_30", "fourth_31", "fourth_32", "fourth_33"]},
+                {"title": "0", "columns": ["first_00", "first_01", "first_02", "first_03"]},
+                {"title": "1", "columns": ["second_10", "second_11", "second_12", "second_13"]},
+                {"title": "0", "columns": ["third_20", "third_21", "third_22", "third_23"]},
+                {"title": "1", "columns": ["fourth_30", "fourth_31", "fourth_32", "fourth_33"]},
             ]
     }
+
     return render_template('index.html', data=data)
 
 if __name__ == "__main__":
